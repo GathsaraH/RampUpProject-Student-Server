eff71f4a990992f271ae1fa39f52209f9495bc01
Gathsara <66924429+GathsaraH@users.noreply.github.com>
2022-02-19 11:09:12 +0530

    Initial Commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+    "indent": ["error", 2],
   },
 };
